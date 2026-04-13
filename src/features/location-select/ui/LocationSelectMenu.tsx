66d1232baee2f0925c741a6863f0e0f3f72b45ce
AbyssParent <artemggr33@gmail.com>
2026-04-13 22:52:14 +0300

    feat(LocationSelectMenu): add key navigation logic

--- src/features/location-select/ui/LocationSelectMenu.tsx
+++ src/features/location-select/ui/LocationSelectMenu.tsx
@@ -1,7 +1,8 @@
 import { LOCATIONS } from '@/features/location-select/config/locations.ts';
 import { createPortal } from 'react-dom';
 import { X } from 'lucide-react';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import * as React from 'react';
 
 interface LocationSelectMenuProps {
   onClose: () => void;
@@ -10,7 +11,11 @@ interface LocationSelectMenuProps {
 }
 
 export const LocationSelectMenu = ({ onClose, onSelect, isOpen }: LocationSelectMenuProps) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
   const inputRef = useRef<HTMLInputElement>(null);
+  const buttonRefs = useRef<(HTMLButtonElement | null)[]>([]);
+  const listRef = useRef<HTMLUListElement>(null);
 
   useEffect(() => {
     if (!isOpen) return;
@@ -27,6 +32,22 @@ export const LocationSelectMenu = ({ onClose, onSelect, isOpen }: LocationSelect
 
     return () => window.removeEventListener('keydown', handleEscapeButtonClose);
   }, [isOpen, onClose]);
+
+  const handleListKeyDown = (e: React.KeyboardEvent) => {
+    if (e.key === 'ArrowDown') {
+      e.preventDefault();
+      const nextIndex = activeIndex + 1 === LOCATIONS.length ? 0 : activeIndex + 1;
+      setActiveIndex(nextIndex);
+      buttonRefs.current[nextIndex]?.focus();
+    }
+
+    if (e.key === 'ArrowUp') {
+      e.preventDefault();
+      const nextIndex = activeIndex - 1 < 0 ? LOCATIONS.length - 1 : activeIndex - 1;
+      setActiveIndex(nextIndex);
+      buttonRefs.current[nextIndex]?.focus();
+    }
+  };
 
   return createPortal(
     <>
@@ -62,14 +83,21 @@ export const LocationSelectMenu = ({ onClose, onSelect, isOpen }: LocationSelect
             ref={inputRef}
           />
 
-          <ul className="flex flex-col gap-2 mt-10 max-h-106 overflow-y-auto scroll-smooth">
-            {LOCATIONS.map(({ label, value }) => (
+          <ul
+            className="flex flex-col gap-2 mt-10 max-h-106 overflow-y-auto scroll-smooth"
+            ref={listRef}
+            onKeyDown={(e) => handleListKeyDown(e)}
+          >
+            {LOCATIONS.map(({ label, value }, index) => (
               <li className="hover:bg-gray" key={value}>
                 <button
                   aria-label="Выбрать город"
                   onClick={() => onSelect(label)}
                   className="cursor-pointer w-full text-left py-2"
                   type="button"
+                  ref={(el) => {
+                    buttonRefs.current[index] = el;
+                  }}
                 >
                   {label}
                 </button>
