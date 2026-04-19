cddc2cbbf4681b81d560d69730cddb4ff31e6ea0
AbyssParent <artemggr33@gmail.com>
2026-04-19 10:30:00 +0300

    feat(hero): add slider pagination with a11y support. Finish hero section

--- src/shared/ui/slider/Slider.tsx
+++ src/shared/ui/slider/Slider.tsx
@@ -1,5 +1,6 @@
 import { Swiper, type SwiperProps } from 'swiper/react';
 import type { ReactNode } from 'react';
+import { A11y } from 'swiper/modules';
 
 interface SliderProps extends SwiperProps {
   children: ReactNode;
@@ -7,7 +8,17 @@ interface SliderProps extends SwiperProps {
 
 export const Slider = ({ children, ...props }: SliderProps) => {
   return (
-    <Swiper speed={800} {...props}>
+    <Swiper
+      modules={[A11y]}
+      a11y={{
+        prevSlideMessage: 'Предыдущий слайд',
+        nextSlideMessage: 'Следующий слайд',
+        firstSlideMessage: 'Это первый слайд',
+        lastSlideMessage: 'Это последний слайд',
+      }}
+      speed={800}
+      {...props}
+    >
       {children}
     </Swiper>
   );
