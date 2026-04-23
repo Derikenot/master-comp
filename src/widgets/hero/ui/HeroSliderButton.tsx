a2daab6b853b46f8938ab8dd8e308dbb7e60280c
AbyssParent <6adobrianskiy@gmail.com>
2026-04-23 18:29:05 +0300

    feat(hero): hide navigation buttons on mobile

--- src/widgets/hero/ui/HeroSliderButton.tsx
+++ src/widgets/hero/ui/HeroSliderButton.tsx
@@ -7,7 +7,7 @@ export const HeroSliderButton = ({ dir, classname, onClick }: SliderButtonProps)
     <SliderButton
       dir={dir}
       classname={cn(
-        'flex items-center justify-center w-11 h-11 bg-primary p-1 rounded-full transition duration-200 hover:bg-gray active:bg-primary',
+        'hidden md:flex items-center justify-center w-11 h-11 bg-primary p-1 rounded-full transition duration-200 hover:bg-gray active:bg-primary',
         classname,
       )}
       onClick={onClick}
